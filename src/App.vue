21091207c9efdd9c1a9b5cc662907ec3a8dfd98a
Jose Castelo <jccb15@gmail.com>
2018-06-30 11:24:13 -0700

    set initial interface

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,85 @@
 <template>
   <v-app>
-    <v-navigation-drawer
-      persistent
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      enable-resize-watcher
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile
-          value="true"
-          v-for="(item, i) in items"
-          :key="i"
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
+
+    <v-navigation-drawer clipped fixed v-model="drawer" app>
+      <v-container>
+        <v-layout row justify-space-between>
+          <v-flex xs10>
+            <v-subheader class="mt-2 grey--text text--darken-1">YEAR</v-subheader>
+          </v-flex>
+          <v-flex xs2>
+            <v-switch v-model="switch1"></v-switch>
+          </v-flex>
+        </v-layout>
+
+        <v-layout row>
+          <v-flex>
+            <v-radio-group v-model="radioGroup">
+              <v-radio label="2011" value="2011"></v-radio>
+              <v-radio label="2012" value="2012"></v-radio>
+              <v-radio label="2013" value="2013"></v-radio>
+              <v-radio label="2014" value="2014"></v-radio>
+            </v-radio-group>
+          </v-flex>
+        </v-layout>
+
+        <v-divider></v-divider>
+
+        <v-layout row justify-space-between>
+          <v-flex xs10>
+            <v-subheader class="mt-2 grey--text text--darken-1">GENDER</v-subheader>
+          </v-flex>
+          <v-flex xs2>
+            <v-switch v-model="switch1"></v-switch>
+          </v-flex>
+        </v-layout>
+
+        <v-layout row>
+          <v-flex>
+            <v-radio-group v-model="radioGroup">
+              <v-radio label="Female" value="0"></v-radio>
+              <v-radio label="Male" value="1"></v-radio>
+            </v-radio-group>
+          </v-flex>
+        </v-layout>    
+
+        <v-divider></v-divider>
+
+        <v-layout row justify-space-between>
+          <v-flex xs10>
+            <v-subheader class="mt-2 grey--text text--darken-1">ETHNICITY</v-subheader>
+          </v-flex>
+          <v-flex xs2>
+            <v-switch v-model="switch1"></v-switch>
+          </v-flex>
+        </v-layout>
+
+        <v-layout row>
+          <v-flex>
+            <v-radio-group v-model="radioGroup">
+              <v-radio label="Hispanic" value="0"></v-radio>
+              <v-radio label="White Non Hispanic" value="1"></v-radio>
+              <v-radio label="Asian & Pacific Islander" value="2"></v-radio>
+              <v-radio label="Black Non Hispanic" value="3"></v-radio>
+            </v-radio-group>
+          </v-flex>
+        </v-layout>
+
+      </v-container>
+ 
+      
     </v-navigation-drawer>
-    <v-toolbar
-      app
-      :clipped-left="clipped"
-    >
+
+   <v-toolbar app fixed clipped-left>
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
+      <v-toolbar-title>Vue NY Test</v-toolbar-title>
     </v-toolbar>
+
     <v-content>
       <HelloWorld/>
     </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile @click="right = !right">
-          <v-list-tile-action>
-            <v-icon>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
-    </v-footer>
+
+
   </v-app>
 </template>
 
@@ -79,13 +93,6 @@ export default {
   },
   data () {
     return {
-      clipped: false,
-      drawer: true,
-      fixed: false,
-      items: [{
-        icon: 'bubble_chart',
-        title: 'Inspire'
-      }],
       miniVariant: false,
       right: true,
       rightDrawer: false,
