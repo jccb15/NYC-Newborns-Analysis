84221842241dacc62c60cce1ce85319f20e7ecba
jose castelo <jccb.15@gmail.com>
2018-07-05 15:48:55 -0700

    Add closing chip feature

--- src/components/AppliedFilters.vue
+++ src/components/AppliedFilters.vue
@@ -1,16 +1,13 @@
 <template>
-  <v-footer
-    height="auto"
-    color="primary lighten-1"
-  >
+  <v-footer height="auto" color="primary lighten-1">
     <v-layout justify-center row wrap>
 
-      <v-chip class="chip" v-for="filter in filters" :key="filter"  close> {{filter}} </v-chip>
+      <v-chip class="chip" v-for="(filter, index) in filters" :key="filter" v-model="activeFilters[index]"  close> {{filter}} </v-chip>
 
       <v-flex primary lighten-2 py-3 text-xs-center white--text xs12>
         &copy;2018 — <strong>Jose Castelo</strong>
       </v-flex>
-      
+
     </v-layout>
   </v-footer>
 </template>
@@ -23,26 +20,23 @@ import { Ethnicity } from '@/components/Data.vue';
 @Component
 export default class AppliedFilters extends Vue {
   filters:string[] = []
-  
-  year!:string
-  gender!:string
-  ethnicity!:string
-
+  activeFilters:boolean[] = []
 
   created(){
     eventBus.$on('filtersChanged', (year:string, gender:string, ethnicity:string) => { 
       this.filters = []
+      this.activeFilters = []
 
-      if(year.length > 0){this.filters.push(year)}
-      if(gender.length > 0){this.filters.push(+gender ? "Male" : "Female" )}
-      if(ethnicity.length > 0){this.filters.push(Ethnicity[+ethnicity].replace(/([A-Z])/g, ' $1').trim())}
+      if(year.length > 0){this.filters.push(year); this.activeFilters.push(true)}
+      
+      if(gender.length > 0){this.filters.push(+gender ? "Male" : "Female" )
+      this.activeFilters.push(true)}
+      
+      if(ethnicity.length > 0){this.filters.push(Ethnicity[+ethnicity].replace(/([A-Z])/g, ' $1').trim())
+      this.activeFilters.push(true)
+      }
     })
   }
-
-  showFilters(){
-
-  }
-
 }
 </script>
 
